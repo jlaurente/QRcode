2604cbbbf781d79620c0ab984c724afcb5391d10
kagame_01 <laurente.juvy13@gmail.com>
2017-09-12 14:37:36 +0800

    QCode

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,10 @@
 var resultDiv;
+var resultAge;
 document.addEventListener("deviceready", init, false);
 function init() {
     document.querySelector("#startScan").addEventListener("touchend", startScan, false);
     resultDiv = document.querySelector("#results");
+	resultAge = document.querySelector("#age");
 }
 function startScan() {
     cordova.plugins.barcodeScanner.scan(
@@ -14,13 +16,14 @@ function startScan() {
 			  //fetch clue sequence from barcode
 			  var mycluesequence = obj.age;
 			  //form JSON string
-			  var myscanJSON = '{"name":"' + myeventid + '","age":"' + mycluesequence + '"}';
+			  //var myscanJSON = '{"name":"' + myeventid + '","age":"' + mycluesequence + '"}';
 			 
             //var s = "Result: " + result.text + "<br/>" +
             //"Format: " + result.format + "<br/>" +
             //"Cancelled: " + result.cancelled;
 			
-            resultDiv.innerHTML = myscanJSON;
+            resultDiv.innerHTML = myeventid;
+			resultAge.innerHTML = mycluesequence;
 			
         },
         function (error) {
