6e15ad1f98316059cff47be4ebe7f8a089549a8b
kagame_01 <laurente.juvy13@gmail.com>
2017-09-12 14:22:40 +0800

    Android

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,9 @@
-var resultDiv;
+var myscan = null;
+var myclueJSON = null;
+
+var myscan = scanner.scan(startScan(result), handleError(error)); //call scanning function and assign result JSON to myscan variable
+alert(myscan); //returns undefined
+
 document.addEventListener("deviceready", init, false);
 function init() {
     document.querySelector("#startScan").addEventListener("touchend", startScan, false);
@@ -7,10 +12,17 @@ function init() {
 function startScan() {
     cordova.plugins.barcodeScanner.scan(
         function (result) {
-            var s = "Result: " + result.text + "<br/>" +
-            "Format: " + result.format + "<br/>" +
-            "Cancelled: " + result.cancelled;
-            resultDiv.innerHTML = s;
+             var myresult = result.text;
+			  var obj= JSON.parse(myresult);
+			  //fetch event id from barcode
+			  var myeventid = obj.name;
+			  //fetch clue sequence from barcode
+			  var mycluesequence = obj.age;
+			  //form JSON string
+			  var myscanJSON = '{"name":"' + myeventid + '","age":"' + mycluesequence + '"}';
+			  //return JSON string
+			  return myscanJSON;
+			
         },
         function (error) {
             alert("Scanning failed: " + error);
